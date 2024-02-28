1e170647b82761b92f026f01deb2aef5a6869c0d
sreeragh <sreeragh.s@icloud.com>
2024-02-29 04:35:08 +0530

    commit

---
+++ src/app/components/Content.tsx
@@ -0,0 +1,221 @@
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { AiOutlineClose, AiOutlinePlus } from "react-icons/ai";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+
+const propertiesSchema = z.object({
+  heading: z.string(),
+  description: z.string(),
+  image: z.string(),
+  headingspan: z.string().optional(),
+  afterheadingspan: z.string().optional(),
+  alt: z.string().optional(),
+  id: z.string().optional(),
+  buttons: z
+    .array(
+      z.object({
+        name: z.string(),
+        url: z.string(),
+      })
+    )
+    .default([])
+    .optional(),
+});
+
+type propertiesSchemaType = {
+  heading: string;
+  description: string;
+  image: string;
+  headingspan: string;
+  afterheadingspan: string;
+  id: string;
+  alt: string;
+  buttons: ButtonType[];
+};
+
+type ButtonType = {
+  name: string;
+  url: string;
+};
+
+interface ContentProps {
+  formData: propertiesSchemaType; // Receive formData from props
+  updateFormData: (newFormData: propertiesSchemaType) => void; // Receive updateFormData function from props
+}
+
+export function Content({ formData, updateFormData }: ContentProps) {
+  const form = useForm<propertiesSchemaType>({
+    resolver: zodResolver(propertiesSchema),
+    mode: "onBlur",
+    defaultValues: formData, // Set default values from props
+  });
+
+  function applyChanges(values: propertiesSchemaType) {
+    updateFormData(values); // Call updateFormData function from props with updated values
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        className="space-y-3 mt-5"
+        onBlur={form.handleSubmit(applyChanges)}
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <FormField
+          control={form.control}
+          name="heading"
+          render={({ field }) => (
+            <FormItem>
+              <div className="space-y-0.5"></div>
+              <FormLabel>Heading</FormLabel>
+              <FormControl>
+                {/* <Tiptap onBlur={field.onChange} value={field.value}  /> */}
+                <Input
+                  {...field}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      e.currentTarget.blur();
+                    }
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <div className="space-y-0.5"></div>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                {/*      
+               <Tiptap onBlur={field.onChange} value={field.value}  /> */}
+                <Input
+                  {...field}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      e.currentTarget.blur();
+                    }
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="image"
+          render={({ field }) => (
+            <FormItem>
+              <div className="space-y-0.5"></div>
+              <FormLabel>Image</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      e.currentTarget.blur();
+                    }
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Separator />
+
+        <FormField
+          control={form.control}
+          name="buttons"
+          render={({ field }) => (
+            <FormItem>
+              <div className="flex justify-between items-center">
+                <FormLabel>Buttons</FormLabel>
+                <Button
+                  variant={"outline"}
+                  className="gap-2"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    form.setValue("buttons", [
+                      ...field.value,
+                      { name: "New button", url: "" }, // Set up a new button with name and empty URL
+                    ]);
+                  }}
+                >
+                  <AiOutlinePlus />
+                  Add
+                </Button>
+              </div>
+              <div className="flex flex-col gap-2">
+                {form.watch("buttons").map((button, index) => (
+                  <div
+                    className="flex gap-1 items-center justify-between"
+                    key={index}
+                  >
+                    <Input
+                      placeholder="Button name"
+                      value={button.name}
+                      onChange={(e) => {
+                        const newButtons = [...field.value];
+                        newButtons[index].name = e.target.value;
+                        field.onChange(newButtons);
+                      }}
+                    />
+
+                    <Input
+                      placeholder="Button URL"
+                      value={button.url}
+                      onChange={(e) => {
+                        const newButtons = [...field.value];
+                        newButtons[index].url = e.target.value;
+                        field.onChange(newButtons);
+                      }}
+                    />
+
+                    <Button
+                      variant={"ghost"}
+                      size={"icon"}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        const newButtons = [...field.value];
+                        newButtons.splice(index, 1);
+                        field.onChange(newButtons);
+                      }}
+                    >
+                      <AiOutlineClose />
+                    </Button>
+                  </div>
+                ))}
+              </div>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
+
+export default Content;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,67 @@
-import Image from "next/image";
+'use client'
+import React, { useState } from "react";
+import RightSideBar from "./components/Sidebar";
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    heading: 'Welcome to',
+    description: 'Anim aute id magna aliqua ad ad non deserunt sunt.ua ad ad non deserunt sunt...',
+    image: 'https://liftlearning.com/wp-content/uploads/2020/09/default-image-300x169.png',
+    headingspan: 'Hero Title Span',
+    afterheadingspan: '',
+    id: '',
+    alt: 'Image Alt',
+    buttons: [{ name: 'Contact', url: '' }]
+  });
+
+  const updateFormData = (newFormData: any) => {
+    setFormData(newFormData);
+  };
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="bg-black h-10 border border-red-500 mr-80">
+        <div id={formData.id} className="p-4 max-w-8xl mx-auto bg-white">
+          <div className="my-10 mx-auto max-w-7xl px-4   flex gap-3 lg:flex-justify lg:flex flex-col lg:flex-row">
+            <div className="sm:text-center lg:text-left">
+              <h1
+                dangerouslySetInnerHTML={{ __html: formData.heading }}
+                className="text-4xl tracking-tight font-bold text-black sm:text-5xl md:text-6xl"
+              ></h1>
+              <p
+                dangerouslySetInnerHTML={{ __html: formData.description }}
+                className="mt-3 text-black sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+              ></p>
+
+              {/* <!-- Button Section --> */}
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                {formData.buttons.map((button, index) => (
+                  <div key={index} className="rounded-md shadow mr-4">
+                    {" "}
+                    {/* Added margin */}
+                    <a
+                      href={button.url}
+                      className="w-full flex items-center justify-center px-4 py-3 text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-600 md:py-2 md:text-lg md:px-3"
+                    >
+                      {button.name}
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className="lg:inset-y-0 lg:right-0 lg:w-1/2 my-4 ">
+              <img
+                className="h-56 w-full rounded-lg object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+                src={formData.image}
+                alt={formData.alt}
+              />
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <RightSideBar formData={formData} updateFormData={updateFormData} />
+    </div>
   );
 }
